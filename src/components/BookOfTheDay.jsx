1f8f14fc676fc2a361935ef4a3770982963327f5
Joy He <joyhe520@gmail.com>
2022-12-09 12:07:34 -0600

    home page design in progress

--- src/components/BookOfTheDay.jsx
+++ src/components/BookOfTheDay.jsx
@@ -1,9 +1,13 @@
 import React, {useEffect} from 'react'
+import { Box } from '@mui/system';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+
+
+
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 //!book cover search api
 //https://bookcover-api.onrender.com/bookcover?book_title=Harry+Potter+Destroy+Horcruxes&author_name=Terrance+Crawford
@@ -18,6 +22,8 @@ const theme = createTheme({
   },});
 
 
+
+
 const BookOfTheDay = () => {
 
     useEffect(() => {
@@ -26,9 +32,18 @@ const BookOfTheDay = () => {
   return (
     <>
     <ThemeProvider theme={theme}>
-        <div>
-            <Typography variant="h4">Hello world</Typography>
-        </div>
+        <Box className="container" sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}} >
+            
+            <Box className='quiz' sx={{ background: '#F5EBE0', width: 1/2}}>
+                <Typography variant="h4" align="center" sx={{ mt: '2rem'}}>Take a quick quiz</Typography>
+                
+                <Typography variant="h4" align="center">get your Book of the Day</Typography>
+            </Box>
+            
+            <Box className="showABook" sx={{ background: '#EDEDE9', width: 1/2}}>
+                <Typography variant="h4" align="center" sx={{ mt: '2rem'}}>This is where to display a book</Typography>
+            </Box>
+        </Box>
     </ThemeProvider>
     
     </>
